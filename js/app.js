b82dcc6ba4e8eb44cf45de3b0ae3f28c5dced003
RMABID <mdabidaliimran@gmail.com>
2024-09-23 19:38:02 +0600

    home section commit

--- js/app.js
+++ js/app.js
@@ -45,8 +45,13 @@ feniDonateButton.addEventListener('click', function(){
 
     const totalAmount = parseFloat(document.getElementById('total-amount').innerText);
     const inputDonateAmount = parseFloat(document.getElementById('input-donate-feni').value);
+    const totalDonate = parseFloat(document.getElementById('donate-feni-show').innerText);
 
-    const totalDonateShow = inputDonateAmount;
+    if(inputDonateAmount > totalAmount || isNaN(inputDonateAmount)){
+        return alert('Your Amount  Please try again ')
+    }
+
+    const totalDonateShow = inputDonateAmount + totalDonate;
     const totalAmountDecrease = totalAmount - inputDonateAmount;
 
     document.getElementById('donate-feni-show').innerText = totalDonateShow;
@@ -72,8 +77,13 @@ quotaDonateButton.addEventListener('click', function(){
 
     const totalAmount = parseFloat(document.getElementById('total-amount').innerText);
     const inputDonateAmount = parseFloat(document.getElementById('input-donate-quota').value);
+    const totalDonate = parseFloat(document.getElementById('donate-quota-show').innerText);
 
-    const totalDonateShow = inputDonateAmount;
+    if(inputDonateAmount > totalAmount || isNaN(inputDonateAmount)){
+        return alert('Your Amount  Please try again ')
+    }
+
+    const totalDonateShow = inputDonateAmount + totalDonate;
     const totalAmountDecrease = totalAmount - inputDonateAmount;
 
 
@@ -126,6 +136,34 @@ donateButton.addEventListener('click', function(){
 
      document.getElementById('donate-page-section').classList.remove('hidden')
     document.getElementById('history-list').classList.add('hidden')
+
+});
+
+
+
+const homeButtonEl = document.getElementById('home-btn');
+const blogButtonEl = document.getElementById('blog-btn');
+blogButtonEl.addEventListener('click', function(){
+
+
+
+
+    document.getElementById('donate-page-section').classList.add('hidden');
+    document.getElementById('home-btn').classList.remove('hidden');
+    document.getElementById('blog-btn').classList.add('hidden');
+    document.getElementById('page-btn').classList.add('hidden')
+    document.getElementById('blog-page').classList.remove('hidden')
+
+
+});
+
+homeButtonEl.addEventListener('click', function(){
+
+    document.getElementById('donate-page-section').classList.remove('hidden');
+    document.getElementById('home-btn').classList.add('hidden');
+    document.getElementById('blog-btn').classList.remove('hidden');
+
+
 
 });
 
