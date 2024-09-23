35029eaab1c54f6af6fcea07e447da4d96305081
RMABID <mdabidaliimran@gmail.com>
2024-09-23 16:53:16 +0600

    donate section commit

--- js/app.js
+++ js/app.js
@@ -4,23 +4,41 @@
 
 
 
+// Donate for Flood at Noakhali, Bangladesh section
+
 const donateButtonEl = document.getElementById('donate-button');
 donateButtonEl.addEventListener('click', function(){
 
-    const inputDonateAmount = parseFloat(document.getElementById('input-donate-amount').value);
-
     const totalAmount = parseFloat(document.getElementById('total-amount').innerText);
-    const totalDonate = document.getElementById('donate-amount-show').innerText;
+    const inputDonateAmount = parseFloat(document.getElementById('input-donate-amount').value);
+    const totalDonate = parseFloat(document.getElementById('donate-amount-show').innerText);
     
-    const totalDonateShow = inputDonateAmount;
+    if(inputDonateAmount > totalAmount || isNaN(inputDonateAmount)){
+        return alert('Your Amount  Please try again ')
+    }
+
+    const totalDonateShow = inputDonateAmount + totalDonate;
     const totalAmountDecrease = totalAmount - inputDonateAmount;
+    
     
     document.getElementById('donate-amount-show').innerText = totalDonateShow;
     document.getElementById('total-amount').innerText = totalAmountDecrease;
 
+    const historyItem = document.createElement('div');
+    historyItem.classList = 'bg-white p-8 border rounded-lg mt-4 mb-3'
+    historyItem.innerHTML = `
+         <h1 class="text-xl font-bold text-black"> ${inputDonateAmount} Taka is Donated for famine-2024 at Feni, Bangladesh</h1>
+          <h1>${new Date().toLocaleTimeString()} </h1>
+    `
 
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+
+    
 
 });
+
+// Donate for Flood Relief in Feni,Bangladesh section
 
 const feniDonateButton = document.getElementById('feni-donate-button');
 feniDonateButton.addEventListener('click', function(){
@@ -34,8 +52,20 @@ feniDonateButton.addEventListener('click', function(){
     document.getElementById('donate-feni-show').innerText = totalDonateShow;
     document.getElementById('total-amount').innerText = totalAmountDecrease;
 
+    const historyItem = document.createElement('div');
+    historyItem.classList = 'bg-white p-8 border rounded-lg mt-4 mb-3'
+    historyItem.innerHTML = `
+         <h1 class="text-xl font-bold text-black"> ${inputDonateAmount} Taka is Donated for Flood Relief in Feni,Bangladesh</h1>
+          <h1>${new Date().toLocaleTimeString()} </h1>
+    `
+
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
+
 });
 
+
+// Aid for Injured in the Quota Movement section
 
 const quotaDonateButton = document.getElementById('quota-donate-button');
 quotaDonateButton.addEventListener('click', function(){
@@ -49,4 +79,53 @@ quotaDonateButton.addEventListener('click', function(){
 
     document.getElementById('donate-quota-show').innerText = totalDonateShow
     document.getElementById('total-amount').innerText = totalAmountDecrease;
+
+    const historyItem = document.createElement('div');
+    historyItem.classList = 'bg-white p-8 border rounded-lg mt-4 mb-3'
+    historyItem.innerHTML = `
+         <h1 class="text-xl font-bold text-black"> ${inputDonateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</h1>
+          <h1>${new Date().toLocaleTimeString()} </h1>
+    `
+
+    const historyContainer = document.getElementById('history-list');
+    historyContainer.insertBefore(historyItem, historyContainer.firstChild)
 });
+
+
+
+const donateButton = document.getElementById('donate-btn');
+const historyButton = document.getElementById('history-button');
+historyButton.addEventListener('click', function(){
+
+    historyButton.classList.add(
+       'bg-[#B4F461]'
+       
+    );
+
+    donateButton.classList.remove(
+        'bg-[#B4F461]'
+    )
+
+
+
+    document.getElementById('donate-page-section').classList.add('hidden')
+    document.getElementById('history-list').classList.remove('hidden')
+
+});
+
+donateButton.addEventListener('click', function(){
+
+    donateButton.classList.add(
+        'bg-[#B4F461]'
+    );
+
+    historyButton.classList.remove(
+        'bg-[#B4F461]'
+        
+     );
+
+     document.getElementById('donate-page-section').classList.remove('hidden')
+    document.getElementById('history-list').classList.add('hidden')
+
+});
+
